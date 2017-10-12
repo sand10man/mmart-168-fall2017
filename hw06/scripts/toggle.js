2086d8ab90e5d04020f7202b0476d4ec2b8c823e
sand10man <31373893+sand10man@users.noreply.github.com>
2017-10-11 21:52:30 -0700

    work on homework06 to be continued

--- hw06/scripts/toggle.js
+++ hw06/scripts/toggle.js
@@ -5,9 +5,26 @@
 // toggleLike should find the DOM element with the id 'like'
 // Now, write a conditional statement. It should check to see if the classList
 // on the element contains the 'active' class
-// If it does, remove the 'active' class. If it doesn't add the 'active' class.
+// If it does, remove the 'active' class. If it doesn't add the 'active' class
 
-
+const toggleLikeExperiment =() => {
+if (document.querySelector('.like').classList.contains ('active'))
+    (document.querySelectorAll('.like').forEach(function)(elem) {
+        elem.classList.remove('active')
+      })
+  } else {
+    document.querySelectorAll('.like').forEach(function)(elem) {
+        elem.classList.add('active')
+    })
+  }
+}
+const toggleLIke = () => {
+    if (document.getElementById('like').classList.contains('active')) {
+       document.getElementById('like').classList.remove('active')
+    } else {
+        (document.getElementById('like').classList.add('active')
+    }
+}
 // ------------------------------------
 // PART 2
 // ------------------------------------
@@ -15,3 +32,6 @@
 // It should find a DOM element with the id 'like'
 // Without writing a conditional statement,
 // use the classList to toggle the 'active class'
+const toggle = () => {
+  document.getElementById('like').classList.toggle('active')
+}
