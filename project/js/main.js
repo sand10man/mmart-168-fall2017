e994428cd2e8851f6824e53745ec4f058351cd78
sand10man <31373893+sand10man@users.noreply.github.com>
2017-12-02 21:01:09 -0800

    project commit

--- project/js/main.js
+++ project/js/main.js
@@ -1,28 +1,55 @@
 //Steve: final project: working with Bart GPS info into GoogleMaps
+//Was very tricky to get everything for the map working in proper order
 
 const apiKey = 'MW9S-E7SL-26DU-VV8V'
-let stationsP = null
+let locations
+console.log('https://api.bart.gov/api/stn.aspx?key=' + apiKey +
+                '&cmd=stns&json=y')
 const makeStationList = () => {
-    const url = 'https://api.bart.gov/api/stn.aspx?key=' + apiKey +
-                    '&cmd=stns&json=y'
+    const url = `https://api.bart.gov/api/stn.aspx?key=${apiKey}&cmd=stns&json=y`
     fetch(url)
         .then((response) => {
+            // console.log(response)
             return response.json()
-        })
+
+        }).catch((error) => {console.log(error)})
         .then((json) => {
             //Now do some godd stuff with your new data
+            // console.log(json)
             const locations = []
             json = json.root
             json.stations.station.forEach((station) => {
-              locations.push {'Lat ' + station.gtfs_latitude,
-              'Lng ' + station.gtfs_longitude}
-              console.log(location[])
+              locations.push({
+                lat: +station.gtfs_latitude,
+                lng: +station.gtfs_longitude
             })
-          }
-            //stationsP = json.stations.station
+           console.log(locations)
+            })
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: new google.maps.LatLng (locations[0].lat,
+                  locations[0].lng),
+                zoom: 12, //1-20
+                mapTypeId: google.maps.MapTypeId.SATELLITE
+            })
+            locations.forEach((location) => {
+                console.log('nowhere')
+                var marker = new google.maps.Marker({
+                    position: location,
+                    map: map
+                })
+            })
 
+          })
+        }
+
+        let map = nul
+        var initMap = function () {
+              makeStationList()
+              console.log('gothere')
+            }
       /*
-            console.log(json)
+            //stationsP = json.stations.station
+              console.log(json)
               json.stations.station.forEach((station) => {
               //console.log(station.name)
               //1. create the element you want to add to the DOM
@@ -42,19 +69,27 @@ const makeStationList = () => {
               document.getElementById('station_list').appendChild(option)
               */
 
+//const locations = [
+  /*
+  {
+      lat: 37.8698168,
+      lng: -122.2720481
 
-let map = null
-const locations = []
-  var initMap = function () {
-        map = new google.maps.Map(document.getElementById('map'), {
-        center: locations[0],
-        zoom: 12, //1-20
-        mapTypeId: google.maps.MapTypeId.SATELLITE
-    })
-    locations.forEach((locations) => {
-        var marker = new google.maps.Marker({
-            position: locations,
-            map: map
-        })
-    })
-}
+  },
+  {
+      lat: 37.77,
+      lng: -122.43
+  },
+  {
+      lat: 37.9,
+      lng: -122.23
+  },
+  {
+      lat: 37.8397651,
+      lng: -122.278
+  }
+]
+*/
+    // var url = 'https://api.bart.gov/api/stn.aspx?key=' + apiKey +
+    //                 '&cmd=stns&json=y'
+    // map.data.loadGeoJson(url)
