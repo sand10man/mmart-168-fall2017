5a2a7450a26c8a278a744aca9f8c6a8087891593
sand10man <31373893+sand10man@users.noreply.github.com>
2017-12-12 21:19:46 -0800

    final commit project

--- project/js/main.js
+++ project/js/main.js
@@ -5,6 +5,7 @@ const apiKey = 'MW9S-E7SL-26DU-VV8V'
 let locations
 console.log('https://api.bart.gov/api/stn.aspx?key=' + apiKey +
                 '&cmd=stns&json=y')
+//asynchronous call to BART API
 const makeStationList = () => {
     const url = `https://api.bart.gov/api/stn.aspx?key=${apiKey}&cmd=stns&json=y`
     fetch(url)
@@ -14,8 +15,12 @@ const makeStationList = () => {
 
         }).catch((error) => {console.log(error)})
         .then((json) => {
-            //Now do some godd stuff with your new data
-            // console.log(json)
+            /*
+            Here we get the longitude and latitude for each BART stations
+            locations is an array that is populated by objects of latitude
+            and longitude.
+            the info from BART is string and muct be converted to numeric
+            */
             const locations = []
             json = json.root
             json.stations.station.forEach((station) => {
@@ -25,29 +30,44 @@ const makeStationList = () => {
             })
            console.log(locations)
             })
+            /*
+            Here the BART data that was put into an array of objects in order to
+            form it GoogleMaps consumption. This map funtion is embedded within
+            the main makeStationList function
+            */
             map = new google.maps.Map(document.getElementById('map'), {
                 center: new google.maps.LatLng (locations[0].lat,
                   locations[0].lng),
                 zoom: 12, //1-20
                 mapTypeId: google.maps.MapTypeId.SATELLITE
             })
+            //this is where the markers of the locations of BART stations
+            //are placed onto the GoogleMap
             locations.forEach((location) => {
-                console.log('nowhere')
+                console.log('Locations_markers')
                 var marker = new google.maps.Marker({
                     position: location,
-                    map: map
+                    map: map   //this is necessary to invoke map which we Only
+                              //do after the markers are extablished
                 })
             })
 
           })
         }
 
-        let map = nul
+        //let map = null     This line not neccessary
+
+        /*
+        HTML does a 'callback=initMap, which calls this. Here we convert This
+        call into the main function that we use to access the BART API for station
+        locations and within this function the map function now resides.
+        */
         var initMap = function () {
               makeStationList()
-              console.log('gothere')
+              console.log('main_funtion_call')
             }
-      /*
+
+      /*    Eveertthinf below here is artifact
             //stationsP = json.stations.station
               console.log(json)
               json.stations.station.forEach((station) => {
